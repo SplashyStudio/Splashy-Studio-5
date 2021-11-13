7912806e39629bf7f9d850eb1e18b22e43ca4cfc
MWB6763 <77411288+MWB6763@users.noreply.github.com>
2021-11-13 13:41:20 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const {app, BrowserWindow} = require('electron');
 function createSS5() {
-     const startup = new BrowserWindow({width: 1300, height: 725, frame: false, transparent: true, icon: 'ss5Logo.png', webPreferences: {nodeIntegration: true, devTools: false}});
+     const startup = new BrowserWindow({minWidth: 1300, minHeight: 725, frame: false, transparent: true, icon: 'ss5Logo.png', webPreferences: {nodeIntegration: true, devTools: false}});
      startup.loadFile("startup.html");
      setTimeout(() => {
-          const ss5 = new BrowserWindow({width: 1300, height: 725, frame: false, icon: 'ss5Logo.png', webPreferences: {nodeIntegration: true}});
+          const ss5 = new BrowserWindow({minWidth: 1300, minHeight: 725, frame: false, icon: 'ss5Logo.png', webPreferences: {nodeIntegration: true}});
           ss5.loadFile("ss5.html");
           startup.close();
      }, 2000);
